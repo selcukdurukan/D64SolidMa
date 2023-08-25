6d0736cb40e7bb9b657df37f03550e9bc14d97ea
selcukdurukan <selcukdurukan@outlook.com.tr>
2023-08-25 14:49:52 +0300

    the second commit

--- src/main/java/com/ba/boost/solidDependencyInversionAndOpenClosed/worse/DbLog.java
+++ src/main/java/com/ba/boost/solidDependencyInversionAndOpenClosed/worse/DbLog.java
@@ -3,6 +3,6 @@ package com.ba.boost.solidDependencyInversionAndOpenClosed.worse;
 public class DbLog {
 
     public void Log(String value) {
-        System.out.println(value + " DBLog'a kaydedildi.");
+        System.out.println(value + " DBLog'a kaydedildis.");
     }
 }
