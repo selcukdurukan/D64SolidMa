d37cbad307ba31ca915ddd022d62fa4b07a5846a
selcukdurukan <selcukdurukan@outlook.com.tr>
2023-09-01 18:27:03 +0300

    the sixth commit

--- src/main/java/com/ba/boost/Main.java
+++ src/main/java/com/ba/boost/Main.java
@@ -3,5 +3,6 @@ package com.ba.boost;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!xxxx");
+        System.out.println("hello sixth");
     }
 }
